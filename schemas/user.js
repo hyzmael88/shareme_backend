6e89eefed8942d633e6c83829907f524fd7edf99
hyzmael88 <31554724+hyzmael88@users.noreply.github.com>
2022-11-24 23:08:04 -0600

    Initial commit

---
+++ schemas/user.js
@@ -0,0 +1,17 @@
+export default{
+    name:"user",
+    title: 'User',
+    type: 'document',
+    fields: [
+        {
+            name: 'userName',
+            tittle: 'UserName',
+            type: 'string'
+        },
+        {
+            name: 'image',
+            tittle: 'Image',
+            type: 'string'
+        },
+    ]
+}
